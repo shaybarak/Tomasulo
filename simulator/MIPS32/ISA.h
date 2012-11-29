a51e6dda7644ddb8dd6d3a08beab70061dfd7f51
Shay Barak <shiber@gmail.com>
2012-11-29 19:02:00 +0200

    More ISA consts

--- simulator/MIPS32/ISA.h
+++ simulator/MIPS32/ISA.h
@@ -6,6 +6,8 @@ public:
 	enum Opcode {
 		add, sub, mul, div, addi, subi, lw, sw, beq, bne, slt, slti, j, halt, unknown,
 	};
+	// Size of instruction in bits
+	static const int INSTRUCTION_SIZE = 32;
 	// Count of registers in ISA
-	static const int REGCOUNT = 32;
+	static const int REG_COUNT = 32;
 };
