dee826cd5f940e97c2cf6590302abfe1ebab4393
Shay Barak <shiber@gmail.com>
2012-11-30 11:21:29 +0200

    Main stub

---
+++ simulator/simulator.cpp
@@ -0,0 +1,15 @@
+#include "Configuration\Configuration.h"
+#include "MIPS32\CPU.h"
+#include "MIPS32\InstructionFactory.h"
+#include "MIPS32\LabelAnalyzer.h"
+#include "Output\HexDump.h"
+#include "Output\RegisterDump.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Entry point for the simulator program
+int main(int argc, char** argv) {
+	return 0;
+}
