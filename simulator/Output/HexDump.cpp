cc4bbf8bc61fb98c817775bda28700bb449d4095
Noam <noaminbar510@gmail.com>
2012-12-01 19:26:07 +0200

    Fixes in hexdump store

--- simulator/Output/HexDump.cpp
+++ simulator/Output/HexDump.cpp
@@ -7,7 +7,8 @@ bool HexDump::load(vector<char>& buffer, FILE* in) {
 		// Line is of format:
 		// 00 11 22 33 44 55 66 77
 		int matches = fscanf_s(in, "%02x %02x %02x %02x %02x %02x %02x %02x\n",
-		    input, input+1, input+2, input+3, input+4, input+5, input+6, input+7);
+		                       input, input+1, input+2, input+3, input+4,
+			                   input+5, input+6, input+7);
 		if (matches != 8) {
 			return false;
 		}
@@ -21,10 +22,11 @@ bool HexDump::load(vector<char>& buffer, FILE* in) {
 bool HexDump::store(vector<char>& buffer, FILE* out) {
 	int size = buffer.size();
 	// 8 bytes per line
-	for (int ch = 0; ch < size - 8; ch+=8) {
+	for (int ch = 0; ch <= size - 8; ch+=8) {
 		char* base = &buffer[ch];
 		if (fprintf(out, "%02x %02x %02x %02x %02x %02x %02x %02x\n",
-			        base, base+1, base+2, base+3, base+4, base+5, base+6, base+7) < 0) {
+			        base[0], base[1], base[2], base[3],
+					base[4], base[5], base[6], base[7]) < 0) {
 			return false;
 		}
 	}
