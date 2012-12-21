7e128e3d59050a060bfcca8a927a11122a491273
Shay Barak <shiber@gmail.com>
2012-12-21 23:34:42 +0200

    CPU now uses queue to L1 cache
    Probably works :)

--- simulator/MIPS32/CPU.cpp
+++ simulator/MIPS32/CPU.cpp
@@ -11,37 +11,88 @@ void CPU::loadProgram(vector<Instruction*>* instructions, int instructionsBase, 
 	halted = false;
 }
 
-void onTick(int time) {
-	// Don't execute if halted or if no instructions present
-	if (halted || (instructions == NULL)) {
+void onTick(int now) {
+	this.now = now;
+	// Don't execute if halted
+	if (halted) {
 		return;
 	}
-	if ((pc - instructionBase < 0) ||
-		// (pc-instructionBase) is known to be non-negative at this time
+
+	// If stalled on reading instruction
+	if (instructionReadStall) {
+		int instruction;
+		if (!l1CacheReadQueue->(&instruction, now)) {
+			// Read did not return yet
+			return;
+		}
+		// Verify that instruction was read correctly
+		if (instruction != pc - instructionsBase) {
+			cerr << "CPU exception: illegal opcode " << instruction << "!" << endl;
+			halted = true;
+			return;
+		}
+		instructionReadStall = false;
+		execute(instruction);
+		return;
+	}
+
+	// Otherwise if stalled on reading/writing data
+	if (dataReadStall) {
+		int data;
+		if (!l1CacheReadQueue->(&data, now)) {
+			// Read did not return yet
+			return;
+		}
+		dataStall = false;
+		execute(pc - instructionsBase, data);
+		return;
+	}
+
+	// Otherwise read next instruction
+	if ((pc - instructionsBase < 0) ||
+		// (pc-instructionsBase) is known to be non-negative at this time
 		#pragma warning(disable:4018)
-		(pc - instructionBase >= instructions.size())) {
-		cerr << "CPU exception: program counter out of range!" << endl;
-		halted = true;
-		return;
+		(pc - instructionsBase >= instructions.size())) {
+			cerr << "CPU exception: program counter out of range! PC=" << pc << endl;
+			halted = true;
+			return;
+		}
 	}
+	l1CacheQueue->push(pc, now);
+	instructionReadStall = true;
+}
+
+int CPU::getInstructionsCommitted() const {
+	return instructionsCommitted;
+}
+
+bool CPU::isHalted() const {
+	return halted;
+}
+
+void execute(int instructionIndex) {
 	RTypeInstruction* rtype = NULL;
 	ITypeInstruction* itype = NULL;
 	JTypeInstruction* jtype = NULL;
-	Instruction* instruction = instructions->at(pc - instructionBase);
-	pc++;
-	instructionsCommitted++;
+	Instruction* instruction = instructions->at(pc - instructionsBase);
 	switch (instruction->getOpcode()) {
 	case ISA::add:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
 		(*gpr)[rtype->getRd()] = (*gpr)[rtype->getRs()] + (*gpr)[rtype->getRt()];
+		pc++;
+		instructionsCommitted++;
 		break;
 	case ISA::sub:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
 		(*gpr)[rtype->getRd()] = (*gpr)[rtype->getRs()] - (*gpr)[rtype->getRt()];
+		pc++;
+		instructionsCommitted++;
 		break;
 	case ISA::mul:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
 		(*gpr)[rtype->getRd()] = (*gpr)[rtype->getRs()] * (*gpr)[rtype->getRt()];
+		pc++;
+		instructionsCommitted++;
 		break;
 	case ISA::div:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
@@ -53,92 +104,112 @@ void onTick(int time) {
 			break;
 		}
 		(*gpr)[rtype->getRd()] = (*gpr)[rtype->getRs()] / (*gpr)[rtype->getRt()];
+		pc++;
+		instructionsCommitted++;
 		break;
 	case ISA::slt:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
 		(*gpr)[rtype->getRd()] = ((*gpr)[rtype->getRs()] < (*gpr)[rtype->getRt()]) ? 1 : 0;
+		pc++;
+		instructionsCommitted++;
 		break;
 	case ISA::addi:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		(*gpr)[itype->getRt()] = (*gpr)[itype->getRs()] + itype->getImmediate();
+		pc++;
 		break;
 	case ISA::subi:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		(*gpr)[itype->getRt()] = (*gpr)[itype->getRs()] - itype->getImmediate();
+		pc++;
+		instructionsCommitted++;
 		break;
 	case ISA::slti:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		(*gpr)[itype->getRt()] = ((*gpr)[itype->getRs()] < itype->getImmediate()) ? 1 : 0;
+		pc++;
+		instructionsCommitted++;
 		break;
 	case ISA::lw:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
-		int mem;
-		if (!readMemory((*gpr)[itype->getRs()] + itype->getImmediate(), &mem)) {
+		int address = (*gpr)[itype->getRs()] + itype->getImmediate();
+		if (!isValidAddress(address)) {
 			cerr << "CPU exception: memory offset out of range!" << endl;
 			halted = true;
-			pc--;
-			instructionsCommitted--;
 			break;
 		}
-		(*gpr)[itype->getRt()] = mem;
+		l1CacheReadQueue->push(address, now);
+		// Note: stalled, don't advance PC and don't count instruction as committed
 		break;
 	case ISA::sw:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
-		if (!writeMemory((*gpr)[itype->getRs()] + itype->getImmediate(), (*gpr)[itype->getRt()])) {
+		int address = (*gpr)[itype->getRs()] + itype->getImmediate();
+		int data = (*gpr)[itype->getRt()];
+		if (!isValidAddress(address)) {
 			cerr << "CPU exception: memory offset out of range!" << endl;
 			halted = true;
-			pc--;
-			instructionsCommitted--;
+			break;
 		}
+		pair<int, int> write(address, data);
+		l1CacheWriteQueue->push(write, now);
+		// TODO do write operations stall?
+		pc++;
+		instructionsCommitted++;
 		break;
 	case ISA::beq:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		if ((*gpr)[itype->getRs()] == (*gpr)[itype->getRt()]) {
-			pc += itype->getImmediate();
+			pc += itype->getImmediate() + 1;
+		} else {
+			pc++;
 		}
+		instructionsCommitted++;
 		break;
 	case ISA::bne:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		if ((*gpr)[itype->getRs()] != (*gpr)[itype->getRt()]) {
-			pc += itype->getImmediate();
+			pc += itype->getImmediate() + 1;
+		} else {
+			pc++;
 		}
+		instructionsCommitted++;
 		break;
 	case ISA::j:
 		jtype = dynamic_cast<JTypeInstruction*>(instruction);
 		pc = jtype->getTarget();
+		instructionsCommitted++;
 		break;
 	case ISA::halt:
 		halted = true;
+		instructionsCommitted++;
 		break;
 	default:
 		cerr << "CPU exception: invalid opcode!" << endl;
 		halted = true;
-		pc--;
-		instructionsCommitted--;
 		break;
 	}
 }
 
-int CPU::getInstructionsCommitted() const {
-	return instructionsCommitted;
-}
-
-bool CPU::isHalted() const {
-	return halted;
-}
-
-bool CPU::readMemory(int address, int* value) {
-	if ((address < 0) || (address >= memorySize)) {
-		return false;
+void execute(int instructionIndex, int data) {
+	ITypeInstruction* itype = NULL;
+	Instruction* instruction = instructions->at(pc - instructionsBase);
+	pc++;
+	instructionsCommitted++;
+	// Note: switch only implements lw because other instruction types are not expected
+	switch (instruction->getOpcode()) {
+	case ISA::lw:
+		itype = dynamic_cast<ITypeInstruction*>(instruction);
+		(*gpr)[itype->getRt()] = data;
+		pc++;
+		instructionsCommitted++;
+		break;
+	default:
+		cerr << "CPU exception: invalid opcode!" << endl;
+		halted = true;
+		break;
 	}
-	*value = *(int*)(&memory[address]);
-	return true;
 }
 
-bool CPU::writeMemory(int address, int value) {
-	if ((address < 0) || (address >= memorySize)) {
-		return false;
-	}
-	*(int*)(&memory[address]) = value;
-	return true;
+bool CPU::isValidAddress(int address) {
+	return (address >= 0) && (address < memorySize);
 }
--- simulator/MIPS32/CPU.h
+++ simulator/MIPS32/CPU.h
@@ -3,7 +3,9 @@
 #include "Instruction.h"
 #include "GPR.h"
 #include "../Clock/Clocked.h"
+#include "../Memory/TimedQueue.h"
 #include <vector>
+#include <pair>
 using namespace std;
 
 /**
@@ -17,22 +19,25 @@ public:
 	  * Initialize with a given memory block and GPR.
 	  * Shares memory & GPR with caller.
 	  */
-	CPU(char* memory, int memorySize, GPR* gpr) :
-		memory(memory),
+	CPU(TimedQueue<int>* l1CacheReadQueue, TimedQueue<pair<int, int>>* l1CacheWriteQueue, int memorySize, GPR* gpr) :
+		l1CacheReadQueue(l1CacheReadQueue),
+		l1CacheWriteQueue(l1CacheWriteQueue),
 		memorySize(memorySize),
 		gpr(gpr),
+		now(0),
 		instructionsCommitted(0),
-		executionTime(0).
-		instructions(NULL) {}
+		instructions(NULL),		
+		stalled(false),
+		halted(false) {}
 	/**
 	 * Loads a program.
 	 * instructions: a program represented as a series of instructions
-	 * instructionBase: absolute base memory address at which instructions are mapped
+	 * instructionsBase: absolute base memory address at which instructions are mapped
 	 * pc: value of Program Counter at start of execution
 	 */
-	void loadProgram(vector<Instruction*>* instructions, int instructionBase, int pc);
+	void loadProgram(vector<Instruction*>* instructions, int instructionsBase, int pc);
 	// Clock tick handler
-	virtual void onTick(int time);
+	virtual void onTick(int now);
 	// Returns count of instructions committed so far
 	int getInstructionsCommitted() const;
 	// Returns total execution time
@@ -42,26 +47,43 @@ public:
 
 private:
 	/**
-	 * Reads a machine word from memory.
-	 * address: offset in machine words
-	 * value: output buffer
-	 * Returns whether successful.
+	 * Executes an instruction. May stall on memory.
+	 * instructionIndex: position of instruction in program
 	 */
-	bool readMemory(int address, int* value);
+	void execute(int instructionIndex);
 	/**
-	 * Writes a machine word to memory.
-	 * address: offset in machine words
-	 * value: input buffer
-	 * Returns whether successful.
+	 * Executes an instruction with a given memory word.
+	 * instructionIndex: position of instruction in program
+	 * data: data from memory for instruction
 	 */
-	bool writeMemory(int address, int value);
-	char* memory;
+	void execute(int instructionIndex, int data);
+	/**
+	 * Validates a memory offset.
+	 */
+	bool isValidAddress(int address);
+	// Queue for reading from the L1 cache
+	TimedQueue<int>* l1CacheReadQueue;
+	// Queue for writing to the L1 cache
+	TimedQueue<pair<int, int>>* l1CacheWriteQueue;
+	// Size of memory (such that memorySize == highest valid address + 1)
 	int memorySize;
+	// General {urpose Registers
 	GPR* gpr;
+	// Current cycle
+	int now;
+	// Count of instructions committed
 	int instructionsCommitted;
-	int executionTime;
+	// Program to execute
 	vector<Instruction*>* instructions;
+	// Base address of instructions in memory
 	int instructionsBase;
+	// Program Counter
 	int pc;
+	// Whether stalled on reading instruction from memory
+	bool instructionReadStall;
+	// Whether stalled on reading data from memory
+	bool dataReadStall;
+	// TODO are there write stalls?
+	// Whether halted (by HALT instruction)
 	bool halted;
 };
