6403075e6d8a4dd0c71b350ac1f99732644f2b53
Noam <noaminbar510@gmail.com>
2012-12-22 13:51:28 +0200

    Fix for TimedQueue class: added include .h, fixed signature

--- simulator/Memory/TimedQueue.cpp
+++ simulator/Memory/TimedQueue.cpp
@@ -1,11 +1,13 @@
-void TimedQueue::push(const T& element, int notBefore) {
+#include "TimedQueue.h"
+
+template <typename T> void TimedQueue<T>::push(const T& element, int notBefore) {
 	struct TimedQueue::element elem;
 	elem.value = element;
 	elem.notBefore = notBefore;
 	q.push(elem);
 }
 
-bool TimedQueue::pop(T* element, int notAfter) {
+template <typename T> bool TimedQueue<T>::pop(T* element, int notAfter) {
 	struct TimedQueue::element elem = q.front();
 	if (elem.notBefore <= notAfter) {
 		*element = elem.value;
