1d9a9dc5297df8e8faed4bc0b461313bbb546776
Shay Barak <shiber@gmail.com>
2012-12-29 19:18:00 +0200

    Allow passing back values in the same clock cycle

--- simulator/Clock/Clock.cpp
+++ simulator/Clock/Clock.cpp
@@ -3,8 +3,12 @@
 void Clock::tick() {
 	// Advance clock
 	time++;
-	// Notify observers
-	for (vector<Clocked*>::iterator it = observers.begin() ; it < observers.end(); it++) {
+	// Notify observers back-to-forward
+	for (vector<Clocked*>::iterator it = observers.begin(); it < observers.end(); it++) {
+		(*it)->onTick(time);
+	}
+	// Notify observers forward-to-back (allows passing back values in the same clock cycle)
+	for (vector<Clocked*>::iterator it = observers.rbegin(); it < observers.rend(); it++) {
 		(*it)->onTick(time);
 	}
 }
