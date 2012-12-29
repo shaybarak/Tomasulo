be5163a66c0ec4eb07d56d3f3b34ddc93ece2d7c
Shay Barak <shiber@gmail.com>
2012-12-29 20:42:28 +0200

    Dirty bits for L2 (write-back cache)

--- simulator/Memory/L2Cache.cpp
+++ simulator/Memory/L2Cache.cpp
@@ -1,5 +1,13 @@
 #include "L2Cache.h"
 
+L2Cache::L2Cache(int* buffer, int blockSize, int cacheSize, int accessDelay,
+		PreviousMemoryLevel* previousMemoryLevel, NextMemoryLevel* nextMemoryLevel)
+		: Cache(buffer, blockSize, cacheSize, accessDelay, previousMemoryLevel, nextMemoryLevel) {
+	// Also initialize dirty bits
+	instructionsDirty.resize(instructionsValid.size());
+	dataDirty.resize(dataValid.size());
+}
+
 void L2Cache::onTick(int now) {
 	// TODO
 }
--- simulator/Memory/L2Cache.h
+++ simulator/Memory/L2Cache.h
@@ -5,7 +5,10 @@
 class L2Cache : public Cache, public Clocked {
 public:
 	L2Cache(int* buffer, int blockSize, int cacheSize, int accessDelay,
-		PreviousMemoryLevel* previousMemoryLevel, NextMemoryLevel* nextMemoryLevel)
-		: Cache(buffer, blockSize, cacheSize, accessDelay, previousMemoryLevel, nextMemoryLevel) {}
+		PreviousMemoryLevel* previousMemoryLevel, NextMemoryLevel* nextMemoryLevel);
 	virtual void onTick(int now);
+
+private:
+	vector<bool> instructionsDirty;
+	vector<bool> dataDirty;
 };
