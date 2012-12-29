67b60514cfef2acc06e8472fed3aff32677e600f
Shay Barak <shiber@gmail.com>
2012-12-29 21:47:36 +0200

    Fix #29

--- simulator/Memory/L2Cache.cpp
+++ simulator/Memory/L2Cache.cpp
@@ -11,3 +11,22 @@ L2Cache::L2Cache(int* buffer, int blockSize, int cacheSize, int accessDelay,
 void L2Cache::onTick(int now) {
 	// TODO
 }
+
+int L2Cache::toTag(int address) {
+	// Since there are two ways per block in the cache
+	address / (cacheSize / 4);
+}
+
+int L2Cache::toInstructionBlockNumber(int address, int way) {
+	// The cache is partitioned 50%/50% between instructions and data,
+	// and is direct-mapped.
+	// Ways are sequential in the data cache (first all of way 0, then all of way 1).
+	(address / blockSize) % ((cacheSize / 4) / blockSize) + ((cacheSize / 4) / blockSize * way);
+}
+
+int L2Cache::toDataBlockNumber(int address, int way) {
+	// The cache is partitioned 50%/50% between instructions and data,
+	// and is direct-mapped.
+	// Ways are interleaved in the data cache (way 0, way 1, way 0, way 1...)
+	((address / blockSize) % ((cacheSize / 4) / blockSize)) + way;
+}
--- simulator/Memory/L2Cache.h
+++ simulator/Memory/L2Cache.h
@@ -9,6 +9,12 @@ public:
 	virtual void onTick(int now);
 
 private:
+	// Maps an address to a tag
+	int toTag(int address);
+	// Maps a memory address to a block number (way is 0 or 1)
+	int toInstructionBlockNumber(int address, int way);
+	int toDataBlockNumber(int address, int way);
+
 	vector<bool> instructionsDirty;
 	vector<bool> dataDirty;
 };
