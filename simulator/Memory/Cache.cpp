bb35b58638814f96c6d651c07a2ea87eba030a13
Shay Barak <shiber@gmail.com>
2012-12-29 18:48:44 +0200

    Better handling for valid bits

--- simulator/Memory/Cache.cpp
+++ simulator/Memory/Cache.cpp
@@ -5,7 +5,11 @@ Cache::Cache(int* buffer, int blockSize, int cacheSize, int accessDelay,
 	: buffer(buffer), blockSize(blockSize), cacheSize(cacheSize), accessDelay(accessDelay),
 	  previousMemoryLevel(previousMemoryLevel), nextMemoryLevel(nextMemoryLevel) {
 	instructionsBuffer(cacheSize / 2);
+	// Valid bits are per block
+	instructionsValid(instructionsBuffer.size() / blockSize);
 	instructions = &instructionsBuffer[0];
 	dataBuffer(cacheSize / 2);
+	// Valid bits are per block
+	dataValid(dataBuffer.size() / blockSize);
 	data = &dataBuffer[0];
 }
