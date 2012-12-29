8c9d5cee937eb7bc935864463a15abbd62d956de
Shay Barak <shiber@gmail.com>
2012-12-29 18:54:08 +0200

    Methods for accessing L1 cache buffer

--- simulator/Memory/L1Cache.cpp
+++ simulator/Memory/L1Cache.cpp
@@ -3,3 +3,25 @@
 void L1Cache::onTick(int now) {
 	// TODO
 }
+
+bool L1Cache::read(int offset, int* value) {
+	if (offset < cacheSize / 2) {
+		// Use instructions buffer
+		*value = instructions[offset / sizeof(int)]
+		return instructionsValid[offset / blockSize];
+	} else {
+		offset = offset % (cacheSize / 2);
+		*value = data[(offset / sizeof(int)]
+		return dataValid[offset / blockSize];
+	}
+}
+
+void L1Cache::write(int offset, int value) {
+	if (offset < cacheSize / 2) {
+		// Use instructions buffer
+		instructions[offset / sizeof(int)] = value;
+	} else {
+		offset = offset % (cacheSize / 2);
+		data[(offset / sizeof(int)] = value;
+	}
+}
--- simulator/Memory/L1Cache.h
+++ simulator/Memory/L1Cache.h
@@ -9,4 +9,24 @@ public:
 		PreviousMemoryLevel* previousMemoryLevel, NextMemoryLevel* nextMemoryLevel)
 		: Cache(buffer, blockSize, cacheSize, accessDelay, previousMemoryLevel, nextMemoryLevel) {}
 	virtual void onTick(int now);
+
+private:
+	/**
+	 * Reads a memory word from cache.
+	 * If invalid does not send a read request to the next level.
+	 *
+	 * offset: byte offset
+	 * value: output
+	 * Returns whether valid. 
+	 */
+	bool read(int offset, int* value);
+	
+	/**
+	 * Writes a memory word to cache.
+	 * Does not send a write request to the next level.
+	 *
+	 * offset: byte offset
+	 * value: input
+	 */
+	void write(int offset, int value);
 };
