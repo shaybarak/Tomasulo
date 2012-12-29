4a24f399637609556e91f3f88b2ed72aa202eb63
Noam <noaminbar510@gmail.com>
2012-12-30 00:40:00 +0200

    Cahnged buffer default constructors

--- simulator/Memory/Cache.cpp
+++ simulator/Memory/Cache.cpp
@@ -1,17 +1,17 @@
 #include "Cache.h"
 
 Cache::Cache(int* buffer, int blockSize, int cacheSize, int accessDelay,
-	PreviousMemoryLevel* previousMemoryLevel, NextMemoryLevel* nextMemoryLevel)
-	: buffer(buffer), blockSize(blockSize), cacheSize(cacheSize), accessDelay(accessDelay),
-	  previousMemoryLevel(previousMemoryLevel), nextMemoryLevel(nextMemoryLevel) {
+		PreviousMemoryLevel* previousMemoryLevel, NextMemoryLevel* nextMemoryLevel)
+		: data(buffer), blockSize(blockSize), cacheSize(cacheSize), accessDelay(accessDelay),
+		  previousMemoryLevel(previousMemoryLevel), nextMemoryLevel(nextMemoryLevel) {
 	instructionsBuffer.resize(cacheSize / 2);
 	// Tag and valid bit are per block
 	instructionsTag.resize(instructionsBuffer.size() / blockSize);
 	instructionsValid.resize(instructionsTag.size());
-	instructions = &instructionsBuffer[0];
+	instructions = (int*)&instructionsBuffer[0];
 	dataBuffer.resize(cacheSize / 2);
 	// Tag and valid bit are per block
 	dataTag.resize(dataBuffer.size() / blockSize);
 	dataValid.resize(dataTag.size());
-	data = &dataBuffer[0];
+	data = (int*)&dataBuffer[0];
 }
