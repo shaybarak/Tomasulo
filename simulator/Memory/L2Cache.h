ad741064aef8ad5caa6b6632e0c146b0f410f1e0
Shay Barak <shiber@gmail.com>
2013-01-06 01:10:00 +0200

    Working on leveraging Cache base class in L2Cache

--- simulator/Memory/L2Cache.h
+++ simulator/Memory/L2Cache.h
@@ -12,36 +12,13 @@ public:
 	virtual void onTick(int now);
 
 protected:
-	virtual bool evict(int address);
-	virtual int toTag(int address);
-	virtual int toBlockNumber(int address);
-	virtual int toAddress(int tag, int blockNumber, int blockOffset);
+	virtual outcome isPresent(int addressIn, int* addressOut);
+	virtual void read(int addressIn, int* dataOut);
+	virtual void write(int addressIn, int dataIn);
+	virtual void evict(int address);
 
 private:
 	static int WAYS = 2;
-
-	int toWayInstruction(int blockNumber, int way);
-	int toWayData(int blockNumber, int way);
-	/**
-	 * Reads a memory word from cache.
-	 * If invalid does not send a read request to the next level.
-	 *
-	 * address: byte offset
-	 * value: output - data read if valid or data present in associated LRU way
-	 *        (to evict when read from next level returns)
-	 * addressToEvict: address of data present in associated LRU way if needs evicting
-	 *                 or negative otherwise
-	 * Returns whether valid. 
-	 */
-	bool read(int address, int* value, int* addressToEvict);
-	/**
-	 * Writes a memory word to cache.
-	 * Does not send a write request to the next level.
-	 *
-	 * address: byte offset
-	 * value: input
-	 */
-	void write(int address, int value);
 
 	vector<bool> instructionsDirty;
 	vector<bool> dataDirty;
