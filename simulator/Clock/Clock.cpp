cc0dbe63bfcd90c6ddc642a2d0b5fab6081d6e0e
Shay Barak <shiber@gmail.com>
2013-01-09 00:11:18 +0200

    Fixed calling last observed twice

--- simulator/Clock/Clock.cpp
+++ simulator/Clock/Clock.cpp
@@ -8,7 +8,8 @@ void Clock::tick() {
 		(*it)->onTick(time);
 	}
 	// Notify observers forward-to-back (allows passing back values in the same clock cycle)
-	for (vector<Clocked*>::reverse_iterator rit = observers.rbegin(); rit < observers.rend(); rit++) {
+	// (the last observer is not called to prevent double dispatching)
+	for (vector<Clocked*>::reverse_iterator rit = observers.rbegin()++; rit < observers.rend(); rit++) {
 		(*rit)->onTick(time);
 	}
 }
