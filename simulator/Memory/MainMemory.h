87ad9ede279749356a5d33f18cbf59877fbc510b
Noam <noaminbar510@gmail.com>
2013-01-11 21:52:07 +0200

    MainMemory refactoring to new design

--- simulator/Memory/MainMemory.h
+++ simulator/Memory/MainMemory.h
@@ -13,18 +13,25 @@ public:
 	MainMemory(int accessDelay, int rowSize);
 	// Design flaw: exposes non-const pointer to data member
 	vector<unsigned char>* getBuffer();
-	virtual void onTick(int now);
+	virtual void onTickUp(int now);
+	virtual void onTickDown(int now);
 
 private:
 	vector<unsigned char> buffer;
 	int* words;
 	int accessDelay;
+	//will be used as a down counter for serving the request
+	int delayCountDown; 
 	int rowSize;
+	int openedRow;
 	// For identifying sequential access
 	int lastReadAddress;
 	// Busy reading until this time
 	int busyReadingUntil;
 	
+	int getAddressRow(int address);
+
 	int read(int offset);
 	void write(int offset, int value);
+	bool ready;
 };
