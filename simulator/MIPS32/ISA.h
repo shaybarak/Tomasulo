c9ca566132e433312c6e97f6009c2130a4c93e55
Shay Barak <shiber@gmail.com>
2013-01-12 12:24:26 +0200

    No more need for CODE_BASE
    Given split between instructions/data memory

--- simulator/MIPS32/ISA.h
+++ simulator/MIPS32/ISA.h
@@ -17,8 +17,6 @@ public:
 	static ISA::Opcode toOpcode(const string& opcodeName);
 	// Count of registers in ISA
 	static const int REG_COUNT = 32;
-	// Size of RAM in bytes
-	static const int RAM_SIZE = 16 * 1024 * 1024;
-	// Base address of code
-	static const int CODE_BASE = 15 * 1024 * 1024;
+	// Size of RAM in bytes (not including code segment)
+	static const int RAM_SIZE = 15 * 1024 * 1024;
 };
