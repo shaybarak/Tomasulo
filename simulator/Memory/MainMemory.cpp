fd5850a498fc473ec4252fbffedb7a10bb4993ec
Shay Barak <shiber@gmail.com>
2013-01-12 16:01:16 +0200

    Fixes to main memory bus protocol

--- simulator/Memory/MainMemory.cpp
+++ simulator/Memory/MainMemory.cpp
@@ -29,25 +29,21 @@ void MainMemory::onTickUp(int now) {
 }
 
 void MainMemory::onTickDown(int now) {
-	delayCountDown--;
 	if (!pL2Master->masterReady) {
 		return;
 	}
-	if (delayCountDown != 0) {
-		pL2Master->slaveReady = false;
-		pL2Master->slaveValid = false;
+	if (--delayCountDown > 0) {
 		return;
 	}
 	openedRow = toRow(pL2Master->address);
 	if (pL2Master->writeEnable) {
 		words[pL2Master->address / sizeof(int)] = pL2Master->data;
-		pL2Master->slaveReady = true;
 		pL2Master->slaveValid = false;
 	} else {
 		pL2Master->data = words[pL2Master->address / sizeof(int)];
 		pL2Master->slaveValid = true;
-		pL2Master->slaveReady = true;
 	}
+	pL2Master->slaveReady = true;
 }
 
 int MainMemory::toRow(int address) {
