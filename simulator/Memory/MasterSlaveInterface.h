66ac7e2d7d4fd0fa3def9b34c4ffe9fb7fe31101
Shay Barak <shiber@gmail.com>
2013-01-12 17:58:09 +0200

    Removed masterReady signal
    Not needed anywhere

--- simulator/Memory/MasterSlaveInterface.h
+++ simulator/Memory/MasterSlaveInterface.h
@@ -2,15 +2,13 @@
 
 class MasterSlaveInterface {
 public:
-	MasterSlaveInterface() : slaveReady(true), slaveValid(false),
-		masterReady(false), masterValid(false), writeEnable(false) {}
+	MasterSlaveInterface() : slaveReady(true), slaveValid(false), 
+		masterValid(false), writeEnable(false) {}
 
 	int address;
 	int data;
 	bool slaveReady;
 	bool slaveValid;
-	bool masterReady;
 	bool masterValid;
 	bool writeEnable;
-	//writeEnable + slaveReady => write was succefully committed
 };
