fbcc116bbd44527d9e5fa8cf063f848827807977
Shay Barak <shiber@gmail.com>
2013-01-13 19:32:23 +0200

    Adapting CPU to use new MemorySystem

--- simulator/MIPS32/CPU.cpp
+++ simulator/MIPS32/CPU.cpp
@@ -8,113 +8,67 @@
 void CPU::loadProgram(vector<Instruction*>* instructions, int pc) {
 	this->instructions = instructions;
 	this->pc = pc;
-	state = READY;
 }
 
-void CPU::onTickUp(int now) {
-	this->now = now;
-	switch (state) {
-	case READY:
-		requestReadInst();
-		state = INST_STALL;
-		break;
-	case INST_STALL:
-		break;
-	case LW:
-		if (pL1DataSlave->slaveReady) {
-			requestReadData(nextLwAddress);
-			state = LW_STALL;
-		}
-		break;
-	case LW_STALL:
-		break;
-	case SW_STALL:
-		break;
-	case HALT:
-		break;
-	default:
-		// Unknown state
-		state = HALT;
-		assert(false);
-		break;
-	}
-}
-
-void CPU::onTickDown(int now) {
-	this->now = now;
-	switch (state) {
-	case READY:
-		break;
-	case INST_STALL:
-		if (!pL1InstSlave->slaveValid) {
-			break;
-		}
-		pL1InstSlave->masterValid = false;
-		execute(pL1InstSlave->data);
-		break;
-	case LW:
-		if (!pL1DataSlave->slaveReady) {
-			// Unexpected state: LW on tickDown
-			state = HALT;
-			assert(false);
-			break;
-		}
-		break;
-	case LW_STALL:
-		if (!pL1DataSlave->slaveValid) {
-			break;
-		}
-		continueExecuteLw();
-		pL1DataSlave->masterValid = false;
-		state = READY;
-		break;
-	case SW_STALL:
-		if (pL1DataSlave->slaveReady) {
-			requestWrite(nextSwAddress, nextSwData);
-			continueExecuteSw();
-			pL1DataSlave->masterValid = false;
-			state = READY;
-		}
-		break;
-	case HALT:
-		break;
-	default:
-		// Unknown state
-		state = HALT;
-		assert(false);
-		break;
-	}
-}
-
-void CPU::execute(int instructionIndex) {
-	if (pL1InstSlave->data != pc) {
-		cerr << "CPU exception: illegal opcode " << (pL1InstSlave->data) << "!" << endl;
-		state = HALT;
+void CPU::runOnce() {
+	// Don't execute if halted or if no instructions present
+	if (halted || (instructions == NULL)) {
 		return;
 	}
-	state = READY;
+	// Validate program counter
+	assert(pc >= 0);
+	assert(pc < instructions->size());
+	readInstruction();
+	pc++;
+	execute(instructions->at(pc-1));
+	instructionsCommitted++;
+}
+
+void CPU::readInstruction() {
+	int instruction;
+	// Read from instruction memory
+	now = instructionMemory->read(now, pc * sizeof(int), instruction);
+	memoryAccessCount++;
+	// Verify that instruction was correctly read from memory
+	assert(pc == instruction);
+}
+
+int CPU::readData(int address) {
+	// Verify address is within range
+	assert(address >= 0);
+	assert(address < dataMemorySize);
+	// Read from data memory
+	int data;
+	now = dataMemory->read(now, address, data);
+	memoryAccessCount++;
+	return data;
+}
+
+void CPU::writeData(int address, int data) {
+	// Verify address is within range
+	assert(address >= 0);
+	assert(address < dataMemorySize);
+	// Write to data memory
+	now = dataMemory->write(now, address, data);
+	memoryAccessCount++;
+}
+
+void CPU::execute(Instruction* instruction) {
 	RTypeInstruction* rtype = NULL;
 	ITypeInstruction* itype = NULL;
 	JTypeInstruction* jtype = NULL;
-	Instruction* instruction = instructions->at(instructionIndex);
 	switch (instruction->getOpcode()) {
 	case ISA::add:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
 		(*gpr)[rtype->getRd()] = (*gpr)[rtype->getRs()] + (*gpr)[rtype->getRt()];
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::sub:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
 		(*gpr)[rtype->getRd()] = (*gpr)[rtype->getRs()] - (*gpr)[rtype->getRt()];
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::mul:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
 		(*gpr)[rtype->getRd()] = (*gpr)[rtype->getRs()] * (*gpr)[rtype->getRt()];
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::div:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
@@ -124,138 +78,54 @@ void CPU::execute(int instructionIndex) {
 			break;
 		}
 		(*gpr)[rtype->getRd()] = (*gpr)[rtype->getRs()] / (*gpr)[rtype->getRt()];
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::slt:
 		rtype = dynamic_cast<RTypeInstruction*>(instruction);
 		(*gpr)[rtype->getRd()] = ((*gpr)[rtype->getRs()] < (*gpr)[rtype->getRt()]) ? 1 : 0;
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::addi:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		(*gpr)[itype->getRt()] = (*gpr)[itype->getRs()] + itype->getImmediate();
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::subi:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		(*gpr)[itype->getRt()] = (*gpr)[itype->getRs()] - itype->getImmediate();
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::slti:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		(*gpr)[itype->getRt()] = ((*gpr)[itype->getRs()] < itype->getImmediate()) ? 1 : 0;
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::lw:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
-		nextLwAddress = (*gpr)[itype->getRs()] + itype->getImmediate();
-		nextLwRt = (*gpr)[itype->getRt()];
-		if (!isValidMemoryAddress(nextLwAddress)) {
-			cerr << "CPU exception: memory offset out of range!" << endl;
-			state = HALT;
-			break;
-		}
-		state = LW;
-		// Note: stalled, don't advance PC and don't count instruction as committed
+		int mem = readData((*gpr)[itype->getRs()] + itype->getImmediate());
+		(*gpr)[itype->getRt()] = mem;
 		break;
 	case ISA::sw:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
-		nextSwAddress = (*gpr)[itype->getRs()] + itype->getImmediate();
-		nextSwData = (*gpr)[itype->getRt()];
-		if (!isValidMemoryAddress(nextSwAddress)) {
-			cerr << "CPU exception: memory offset out of range!" << endl;
-			state = HALT;
-			break;
-		}
-		if (pL1DataSlave->slaveReady) {
-			//write buffer enables trying to write on same cycle without waiting access time.
-			requestWrite(nextSwAddress, nextSwData);
-			continueExecuteSw();
-			state = READY;
-			break;
-		}
-		state = SW_STALL;
+		writeData((*gpr)[itype->getRs()] + itype->getImmediate(), (*gpr)[itype->getRt()]);
 		break;
 	case ISA::beq:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		if ((*gpr)[itype->getRs()] == (*gpr)[itype->getRt()]) {
 			pc += itype->getImmediate();
 		}
-		pc++;
-		instructionsCommitted++;
 		break;
 	case ISA::bne:
 		itype = dynamic_cast<ITypeInstruction*>(instruction);
 		if ((*gpr)[itype->getRs()] != (*gpr)[itype->getRt()]) {
-			pc += itype->getImmediate() + 1;
-		} else {
-			pc++;
+			pc += itype->getImmediate();
 		}
-		instructionsCommitted++;
 		break;
 	case ISA::j:
 		jtype = dynamic_cast<JTypeInstruction*>(instruction);
 		pc = jtype->getTarget();
-		instructionsCommitted++;
 		break;
 	case ISA::halt:
-		state = HALT;
-		instructionsCommitted++;
+		halted = true;
 		break;
 	default:
-		cerr << "CPU exception: invalid opcode!" << endl;
-		state = HALT;
+		// Unexpected instruction!
+		assert(false);
 		break;
 	}
-}
-
-//TODO: change to general method on MasterSlaveInterface "MasterRequestRead/Write"
-void CPU::requestReadInst() {
-	pL1InstSlave->writeEnable = false;
-	pL1InstSlave->masterValid = true;
-	pL1InstSlave->address = pcToMemoryOffset(pc);
-	memoryAccessCount++;
-}
-
-void CPU::requestReadData(int address) {
-	pL1DataSlave->writeEnable = false;
-	pL1DataSlave->masterValid = true;
-	pL1DataSlave->address = address;
-	memoryAccessCount++;
-}
-
-void CPU::requestWrite(int address, int data) {
-	pL1DataSlave->writeEnable = true;
-	pL1DataSlave->masterValid = true;
-	pL1DataSlave->address = address;
-	pL1DataSlave->data = data;
-	memoryAccessCount++;
-}
-
-void CPU::continueExecuteLw() {
-	(*gpr)[nextLwRt] = pL1DataSlave->data;
-	pc++;
-	instructionsCommitted++;
-}
-
-void CPU::continueExecuteSw() {
-	pc++;
-	instructionsCommitted++;
-}
-
-bool CPU::isValidMemoryAddress(int address) {
-	return (address >= 0) && (address < memorySize);
-}
-
-bool CPU::isValidInstructionAddress(int address) {
-	return ((address >= 0) && (address < (int)(instructions->size() * sizeof(int))));
-}
-
-int CPU::pcToMemoryOffset(int pc) {
-	return pc * sizeof(int);
 }
--- simulator/MIPS32/CPU.h
+++ simulator/MIPS32/CPU.h
@@ -2,8 +2,7 @@
 
 #include "Instruction.h"
 #include "GPR.h"
-#include "../Clock/Clocked.h"
-#include "../Memory/MasterSlaveInterface.h"
+#include "../Memory/MemorySystem.h"
 #include <vector>
 using namespace std;
 
@@ -12,79 +11,60 @@ using namespace std;
  * No pipelining.
  * Single memory level (no caches).
  */
-class CPU : public Clocked {
+class CPU {
 public:
 	/**
 	  * Initialize with a given memory block and GPR.
 	  * Shares memory & GPR with caller.
 	  */
-	CPU(int memorySize, GPR* gpr, MasterSlaveInterface* pL1InstSlave, MasterSlaveInterface* pL1DataSlave) :
-		memorySize(memorySize),
+	CPU(int dataMemorySize, GPR* gpr, MemorySystem* instructionMemory, MemorySystem* dataMemory) :
+		dataMemorySize(dataMemorySize),
 		gpr(gpr),
-		pL1InstSlave(pL1InstSlave),
-		pL1DataSlave(pL1DataSlave),
-		state(HALT),
+		instructionMemory(instructionMemory),
+		dataMemory(dataMemory),
 		now(0),
 		instructionsCommitted(0),
 		memoryAccessCount(0),
 		instructions(NULL),		
-		instructionReadStall(false),
-		dataReadStall(false),
-		halted(false), 
-		timeStalledOnMemory(0),
-		memoryReadsCount(0) {}
+		halted(false) {}
 	/**
 	 * Loads a program.
 	 * instructions: a program represented as a series of instructions
 	 * pc: value of Program Counter at start of execution, default of zero
 	 */
 	void loadProgram(vector<Instruction*>* instructions, int pc = 0);
-	// Clock tick handlers
-	virtual void onTickUp(int now);
-	virtual void onTickDown(int now);
+	// Executes one instruction
+	virtual void runOnce();
 	// Returns count of instructions committed so far
 	int getInstructionsCommitted() const { return instructionsCommitted; }
 	// Returns the Average Memory Access Time
 	double getAmat() const { return (double)now / (double)memoryAccessCount; }
 	// Returns whether the CPU has halted due to a halt instruction or an exception
-	bool isHalted() const { return state == HALT; }
+	bool isHalted() const { return halted; }
 
 private:
-	enum State {
-		READY,		//initial state, and after execution
-		INST_STALL,	//stalled on instruction read from L1
-		LW,			//after reading lw instrcution
-		LW_STALL,	//stalled on data read from L1
-		SW,			//after reading sw instruction 
-		SW_STALL,	//stalled on data write to L1
-		HALT,		//read halt opcode
-	};
+	// Reads an instruction from the PC
+	void readInstruction();
+
+	// Reads data
+	int readData(int address);
+
+	// Writes data
+	void writeData(int address, int data);
 
 	/**
 	 * Executes an instruction. May stall on memory.
-	 * instructionIndex: position of instruction in program
+	 * instruction: instruction to execute
 	 */
-	void execute(int instructionIndex);
-	/** continue execution of lw when memory call returns **/
-	void continueExecuteLw();
-	/** continue execution of sw when memory call returns **/
-	void continueExecuteSw();
-	/** Returns whether the address is a valid memory offset. */
-	bool isValidMemoryAddress(int address);
-	/** Returns whether the address is a valid memory offset for instructions. */
-	bool isValidInstructionAddress(int address);
-	/** Converts PC value to memory offset. */
-	int pcToMemoryOffset(int pc);
-	/** set memory interface signals for reading instrcution **/
-	void requestReadInst();
-	/** set memory interface signals for reading data **/
-	void requestReadData(int address);
-	/** set memory interface signals for writing instrcution **/
-	void requestWrite(int address, int data);
-	// Size of memory (such that memorySize == highest valid address + 1)
-	int memorySize;
-	// General {urpose Registers
+	void execute(Instruction* instruction);
+	
+	// Size of data memory in bytes (such that memorySize == highest valid address + 1)
+	int dataMemorySize;
+	// General Purpose Registers
 	GPR* gpr;
+	// Memory systems
+	MemorySystem* instructionMemory;
+	MemorySystem* dataMemory;
 	// Current cycle
 	int now;
 	// Count of instructions committed
@@ -97,25 +77,4 @@ private:
 	vector<Instruction*>* instructions;
 	// Program Counter
 	int pc;
-	// Whether stalled on reading instruction from memory
-	bool instructionReadStall;
-	// Whether stalled on reading data from memory
-	bool dataReadStall;
-	//counter for stalled memory time
-	int timeStalledOnMemory;
-	//counter for memory reads count
-	int memoryReadsCount;
-	//Next address to read
-	int nextLwAddress;
-	//Next Rt to write data to
-	int nextLwRt;
-	//Next address to write
-	int nextSwAddress;
-	//Next data to write
-	int nextSwData;
-	//Interfaces for communicating with L1 caches
-	MasterSlaveInterface* pL1DataSlave;
-	MasterSlaveInterface* pL1InstSlave;
-	//CPU internal state
-	State state;
 };
