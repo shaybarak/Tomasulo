9a34d1a02395b74e237cc26ff473df5ab3f0d6be
Shay Barak <shiber@gmail.com>
2013-02-23 14:38:36 +0200

    Trace should take a const string

--- simulator/Output/Trace.cpp
+++ simulator/Output/Trace.cpp
@@ -1,5 +1,5 @@
 #include "Trace.h"
 
-void Trace::write(string& operation, int timeIssued, int timeExecutionStarted, int timeWriteCdb, int timeCommit) {
+void Trace::write(const string& operation, int timeIssued, int timeExecutionStarted, int timeWriteCdb, int timeCommit) {
 	(*out) << operation << "\t" << timeIssued << " " << timeExecutionStarted << " " << timeWriteCdb << " " << timeCommit << endl;
 }
--- simulator/Output/Trace.h
+++ simulator/Output/Trace.h
@@ -9,7 +9,7 @@ public:
 	// Initializes with an output stream to write trace information to.
 	Trace(ostream& out) : out(&out) {}
 	// Writes a trace entry
-	void write(string& operation, int timeIssued, int timeExecutionStarted, int timeWriteCdb, int timeCommit);
+	void write(const string& operation, int timeIssued, int timeExecutionStarted, int timeWriteCdb, int timeCommit);
 
 private:
 	ostream* out;
